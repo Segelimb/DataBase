a54b9cab9fc50511111cc847d689c8ca91476de4
HistLL <history3zpk@gmail.com>
2026-03-27 09:00:20 +0400

    Хз хз работает ли

--- BDcars_Lib/BDcars_Lib/SUBD.cpp
+++ BDcars_Lib/BDcars_Lib/SUBD.cpp
@@ -1,12 +1,29 @@
-#include <iostream>
-#include <fstream>
-#include <string>
 #include "SUBD.h"
+#include <fstream>
+#include <iostream>
+
 using namespace std;
+
+struct ListUser
+{
+    char login[20];
+    char password[20];
+    ListUser* next;
+};
+
+struct ListAuto
+{
+    char vin[18];
+    char brand[7];
+    char model[10];
+    char carcase[15];
+    char drive[10];
+    ListAuto* next;
+};
 
 void PrintAuto(ListAuto* item)
 {
-    setlocale(LC_ALL, "rus");
+    setlocale(LC_ALL, "");
     if (item == nullptr) return; // проверяем корректность указателя
     cout << "Универсальный номер: " << item->vin << endl; // далее выводим данные в консоль
     cout << "Название: " << item->brand << endl;
@@ -18,7 +35,7 @@ void PrintAuto(ListAuto* item)
 
 void PrintListAuto(ListAuto* firstItem)
 {
-    cout << endl;
+    cout << std::endl;
     auto temp = firstItem; // начинаем с первого элемента
     while (temp != nullptr) // перебираем весь список
     {
@@ -43,36 +60,27 @@ void DeleteListAuto(ListAuto*& firstItem)
 // удаление автомобиля по фильтру
 bool DeleteAuto(
     ListAuto*& firstItem,
-    char find[17])
+    char find[18])
 {
     if (firstItem == nullptr) return false;
     auto temp = firstItem;
-    if (temp->vin == find)  // если указатель на начало списка и на удаляемый элемент совпадают, то
+    if (strcmp(temp->vin, find) == 0)  // если указатель на начало списка и на удаляемый элемент совпадают, то
     {                       // частный случай - удаляем первый в списке элемент
         firstItem = firstItem->next; // правим указатель на первый элемент таким образом, чтобы он указывал на следующий в списке
         delete temp; // удаляем первый элемент
-            return true; // его нет, возвращаем false
+        //firstItem = nullptr;
+        return true; // его нет, возвращаем false
     }
     // если удаляемый элемент не первый в списке
-    while (temp->vin != find && temp->next != nullptr) // ищем элемент, предшествующий удаляемому
+    while ((strcmp(temp->next->vin, find) != 0) && temp->next->next != nullptr) // ищем элемент, предшествующий удаляемому
     {
         temp = temp->next; // передвижение к следующему
     }
-    if (temp->next == nullptr) return false;
+    if ((strcmp(temp->next->vin, find) != 0) && temp->next->next == nullptr) return false;
     auto removableItem = temp->next; // иначе обозначили удаляемый элемент, он следующий по списку
     temp->next = removableItem->next;
     delete removableItem;   // удаляем элемент
     return true;
-}
-
-
-
-void Replace(char* str, char what, char value)
-{
-    int len = strlen(str);
-    for (int i = 0; i < len; i++)
-        if (str[i] == what)
-            str[i] = value;
 }
 
 ListAuto* GetLast(ListAuto* firstItem)
@@ -87,9 +95,29 @@ ListAuto* GetLast(ListAuto* firstItem)
     return temp; // возвращаем указатель на последний элемент
 }
 
+void AddFirst(
+    ListAuto*& firstItem,
+    char productVin[18],
+    char productBrand[7],
+    char productModel[10],
+    char productCarcase[15],
+    char productDrive[10])
+{
+    ListAuto* temp = new ListAuto; // создаем в памяти новый элемент списка
+    strcpy_s(temp->vin, productVin);   // присваиваем полям элемента нужные значения
+    strcpy_s(temp->brand, productBrand); // имя продукта - это стока C-Style, поэтому копируем ее с помощью специальной функции копирования строк
+    strcpy_s(temp->model, productModel); // присваиваем полям элемента нужные значения
+    strcpy_s(temp->carcase, productCarcase);
+    strcpy_s(temp->drive, productDrive);// присваиваем полям элемента нужные значения
+    temp->next = firstItem; // нам нужно встроить элемент в начало списка, поэтому
+    // указатель next настраиваем таким образом, чтобы он указывал на элемент
+    // который ранее был первым (на него указывает first)
+    firstItem = temp;       // а теперь делаем, чтобы first указывал на наш созданный элемент
+}
+
 ListAuto* AddLast(
     ListAuto*& firstItem,
-    char productVin[17],
+    char productVin[18],
     char productBrand[7],
     char productModel[10],
     char productCarcase[15],
@@ -115,45 +143,52 @@ ListAuto* AddLast(
     return temp; // возвращаем указатель на последний элемент
 }
 
-void AddFirst(
-    ListAuto*& firstItem,
-    char productVin[17],
-    char productBrand[7],
-    char productModel[10],
-    char productCarcase[15],
-    char productDrive[10])
-{
-    ListAuto* temp = new ListAuto; // создаем в памяти новый элемент списка
-    strcpy_s(temp->vin, productVin);   // присваиваем полям элемента нужные значения
-    strcpy_s(temp->brand, productBrand); // имя продукта - это стока C-Style, поэтому копируем ее с помощью специальной функции копирования строк
-    strcpy_s(temp->model, productModel); // присваиваем полям элемента нужные значения
-    strcpy_s(temp->carcase, productCarcase);
-    strcpy_s(temp->drive, productDrive);// присваиваем полям элемента нужные значения
-    temp->next = firstItem; // нам нужно встроить элемент в начало списка, поэтому
-    // указатель next настраиваем таким образом, чтобы он указывал на элемент
-    // который ранее был первым (на него указывает first)
-    firstItem = temp;       // а теперь делаем, чтобы first указывал на наш созданный элемент
-}
-
 // поиск автомобиля по фильтру
-ListAuto* FindAuto(ListAuto* firstItem, char* filter)
+bool FindAuto(ListAuto* firstItem, int filter, char find[18])
 {
     auto temp = firstItem;
+    bool result = false;
     while (temp != nullptr)
     {
-        if ((strcmp(temp->brand, filter) == 0) || (strcmp(temp->model, filter) == 0) || (strcmp(temp->vin, filter) == 0)) return temp; // возвращаем указатель на найденный элемент
-        temp = temp->next; // переходим к следующему элементу
+        switch (filter) // проверяем какую клавишу нажал пользователь
+        {
+        case 1: // если 1 то вызываем функцию добавления данных
+            if (strcmp(temp->vin, find) == 0)
+            {
+                result = true;
+                cout << endl;
+                PrintAuto(temp);
+            }
+            break;
+        case 2: // если 2 то вызываем функцию вывода данных
+            if (strcmp(temp->brand, find) == 0)
+            {
+                result = true;
+                cout << endl;
+                PrintAuto(temp);
+            }
+            break;
+        case 3: // если 5 то вызываем функцию вывода данных по фильтру
+            if (strcmp(temp->model, find) == 0)
+            {
+                result = true;
+                cout << endl;
+                PrintAuto(temp);
+            }
+            break;
+        }
+        temp = temp->next;
     }
-    return nullptr; // если ничего не нашли - возвращаем nullptr
+    return result; // если ничего не нашли - возвращаем nullptr
 }
 
 // поиск пользователя
-bool FindUser(ListUser* firstItem, char* userName, char* password)
+bool FindUser(ListUser* firstItem, char userName[20], char password[20])
 {
     auto temp = firstItem;
     while (temp != nullptr)
     {
-        if (temp->login == userName && temp->password == password) return true; // возвращаем указатель на найденный элемент
+        if ((strcmp(temp->login, userName) == 0) && (strcmp(temp->password, password) == 0)) return true; // возвращаем указатель на найденный элемент
         temp = temp->next; // переходим к следующему элементу
     }
     return false; // если ничего не нашли - возвращаем nullptr
@@ -162,7 +197,7 @@ bool FindUser(ListUser* firstItem, char* userName, char* password)
 bool SaveListToBinaryFile(ListAuto* firstItem, const std::string& filename)
 {
     if (firstItem == nullptr) return false;
-    std::ofstream f(filename, std::ios::binary);
+    ofstream f(filename, ios::binary);
     if (!f) return false;
     auto temp = firstItem;
     while (temp != nullptr)
@@ -189,36 +224,59 @@ ListAuto* LoadListFromBinaryFile(const string& filename)
     return first;
 }
 
-bool SaveListToTextFile(ListAuto* firstItem, const std::string& filename)
+void AddFirstUser(ListUser*& firstItem, char login[20], char password[20])
 {
-    if (firstItem == nullptr) return false;
-    std::ofstream f(filename);
-    if (!f) return false;
-    auto temp = firstItem;
-    char itemCopy[30];
-    while (temp != nullptr)
-    {
-        strcpy_s(itemCopy, temp->brand);
-        Replace(itemCopy, ' ', '_');
-        f << temp->vin << " " << itemCopy << " " << temp->model
-            << " " << temp->carcase << "\n";
-        temp = temp->next;
-    }
-    bool result = f.good();
-    f.close();
-    return result;
+    ListUser* temp = new ListUser; // создаем в памяти новый элемент списка
+    strcpy_s(temp->login, login);   // присваиваем полям элемента нужные значения
+    strcpy_s(temp->password, password); // имя продукта - это стока C-Style, поэтому копируем ее с помощью специальной функции копирования строк
+
+    temp->next = firstItem; // нам нужно встроить элемент в начало списка, поэтому
+    // указатель next настраиваем таким образом, чтобы он указывал на элемент
+    // который ранее был первым (на него указывает first)
+    firstItem = temp;       // а теперь делаем, чтобы first указывал на наш созданный элемент
 }
 
-ListAuto* LoadListFromTextFile(const std::string& filename)
+ListUser* GetLastUser(ListUser* firstItem)
 {
-    std::ifstream f(filename);
-    if (!f) return nullptr;
-    ListAuto item;
-    ListAuto* first = nullptr;
-    while (f >> item.vin >> item.brand >> item.model >> item.carcase >> item.drive)
+    if (firstItem == nullptr) return nullptr; // если список пуст, то вернем пустой указатель
+    auto temp = firstItem; // временной переменной присваиваем указатель на первый элемент
+    while (temp->next != nullptr) // пока ее поле next не пустой указатель (если пустой, то мы достигли последнего элемента!)
     {
-        Replace(item.brand, '_', ' ');
-        AddLast(first, item.vin, item.brand, item.model, item.carcase, item.drive);
+        // запомните это присваивание - это переход к следующему в списке элементу
+        temp = temp->next;  // перемещаемся к следующему элементу списка
+    }
+    return temp; // возвращаем указатель на последний элемент
+}
+
+ListUser* AddLastUser(ListUser*& firstItem, char login[20], char password[20])
+{
+    if (firstItem == nullptr) // если список пуст, вызовем функцию добавления в начало списка
+    {
+        AddFirstUser(firstItem, login, password);
+        return firstItem;
+    }
+    ListUser* temp = new ListUser; // создаем в памяти новый элемент списка
+    strcpy_s(temp->login, login);   // присваиваем полям элемента нужные значения
+    strcpy_s(temp->password, password); // имя продукта - это стока C-Style, поэтому копируем ее с помощью специальной функции копирования строк
+    temp->next = nullptr; // нам нужно встроить элемент в конец списка, поэтому
+    // указатель next настраиваем таким образом, чтобы он был пустым
+    // который ранее был первым (на него указывает first)
+    GetLastUser(firstItem)->next = temp;    // а теперь делаем, чтобы элемент, который до этого был последним
+    // (а его мы получаем с помощью уже созданной GetLast)
+    // ссылался на наш новый элемент
+    return temp; // возвращаем указатель на последний элемент
+}
+
+
+ListUser* LoadListUserFromBinaryFile(const string& filename)
+{
+    ifstream f(filename, ios::binary);
+    if (!f) return nullptr;
+    ListUser item;
+    ListUser* first = nullptr;
+    while (f.read((char*)&item, sizeof(ListUser)))
+    {
+        AddLastUser(first, item.login, item.password);
     }
     f.close();
     return first;
--- BDcars_Lib/BDcars_Lib/SUBD.h
+++ BDcars_Lib/BDcars_Lib/SUBD.h
@@ -1,20 +1,20 @@
 #pragma once
-struct ListAuto
-{
-    ListAuto* first;
-    char vin[17]; // уникальный номер
-    char brand[7];  // название
-    char model[10];
-    char carcase[15]; // кузов
-    char drive[10]; // привод
-    ListAuto* next; // указатель на следующий элемент
-};
-
 struct ListUser
 {
     char login[20];
     char password[20];
     ListUser* next;
+};
+
+struct ListAuto
+{
+    ListAuto* first;
+    char vin[18]; // уникальный номер
+    char brand[7];  // название
+    char model[10];
+    char carcase[15]; // кузов
+    char drive[10]; // привод
+    ListAuto* next; // указатель на следующий элемент
 };
 
 // печатает элемент списка в консоли O(1)
@@ -26,41 +26,45 @@ void PrintListAuto(ListAuto* firstItem);
 // удаление списка O(n)
 // указатель на первый элемент передаем по ссылке
 void DeleteListAuto(ListAuto*& firstItem);
-bool DeleteAuto(ListAuto*& firstItem, char find[17]);
+bool DeleteAuto(ListAuto*& firstItem, char find[18]);
 
 // Функция получения указателя на последний элемент O(n)
 // Если функция не изменяет указатель на первый элемент, то передаем его по значению
 ListAuto* GetLast(ListAuto* firstItem);
 
-// добавление элемента в конец списка O(n)
-ListAuto* AddLast(
-    ListAuto*& firstItem,
-    char vin[17],
-    char brand[7],
-    char model[10],
-    char carcase[15],
-    char drive[10]
-);
-
 void AddFirst(
     ListAuto*& firstItem,
-    char Vin[17],
+    char Vin[18],
     char Brand[7],
     char Model[10],
     char Carcase[15],
     char Drive[10]
 );
 
+// добавление элемента в конец списка O(n)
+ListAuto* AddLast(
+    ListAuto*& firstItem,
+    char vin[18],
+    char brand[7],
+    char model[10],
+    char carcase[15],
+    char drive[10]
+);
+
 // поиск элемента по названию продукта O(n)
-ListAuto* FindAuto(ListAuto* firstItem, const char* brandName);
+bool FindAuto(ListAuto* firstItem, int filter, char find[18]);
 
 // авторизация пользователя
-bool FindUser(ListUser* firstItem, char userName, char password);
+bool FindUser(ListUser* firstItem, char userName[20], char password[20]);
 
-bool SaveListToBinaryFile(ListAuto* firstItem, const std::string& filename);
+bool SaveListToBinaryFile(ListAuto* firstItem, const string& filename);
 
-ListAuto* LoadListFromBinaryFile(const std::string& filename);
+ListAuto* LoadListFromBinaryFile(const string& filename);
 
-bool SaveListToTextFile(ListAuto* firstItem, const std::string& filename);
+void AddFirstUser(ListUser*& firstItem, char login[20], char password[20]);
 
-ListAuto* LoadListFromTextFile(const std::string& filename);
+ListUser* GetLastUser(ListUser* firstItem);
+
+ListUser* AddLastUser(ListUser*& firstItem, char login[20], char password[20]);
+
+ListUser* LoadListUserFromBinaryFile(const string& filename);
